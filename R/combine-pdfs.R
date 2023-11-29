707fcaf28f79f9e5d78bf7325e9bbd224c4d2685
Stephan Koenig <stephan.koenig@gmail.com>
2023-11-29 15:33:11 -0800

    Rename worksheet

--- R/combine-pdfs.R
+++ R/combine-pdfs.R
@@ -1,7 +1,7 @@
 library(fs)
 library(qpdf)
 
-worksheets <- path("_site", "worksheets", "01_exploring-markup.pdf")
+worksheets <- path("_site", "worksheets", "01_analysis-review.pdf")
 output <- path("_site", "worksheets", "analysis.pdf")
 combined_pdf <- file_temp(ext = "pdf")
 pdf_combine(c(worksheets, output), combined_pdf)
